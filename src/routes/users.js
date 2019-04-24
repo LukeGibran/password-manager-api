19b28f51a286c72a6b36d7c84d538f8643fdc93f
LukeGibran <liukjhivran@gmail.com>
2019-04-24 18:17:09 +0800

    Second commit -- get user, login for user, middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = new express.Router();
+const auth = require('../middleware/auth');
 const User = require('../model/user');
 
 router.post('/users', async (req, res) => {
@@ -12,6 +13,23 @@ router.post('/users', async (req, res) => {
   } catch (error) {
     res.status(500).send(error);
   }
+});
+
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.authenticate(
+      req.body.email,
+      req.body.master_password
+    );
+    const token = await user.generateToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
 });
 
 module.exports = router;
