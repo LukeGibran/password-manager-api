edc67d34f0ba60de84a553aeb67a13c080888b5d
LukeGibran <liukjhivran@gmail.com>
2019-04-27 11:02:33 +0800

    Third commit -- added new routes(update, delete, logout, logout All)

--- src/routes/users.js
+++ src/routes/users.js
@@ -24,12 +24,68 @@ router.post('/users/login', async (req, res) => {
     const token = await user.generateToken();
     res.send({ user, token });
   } catch (error) {
+    res.status(404).send({ error: 'Invalid login credentials!' });
+  }
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => {
+      token.token !== req.token;
+    });
+
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
     res.status(500).send(error);
   }
 });
 
 router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
+});
+
+router.patch('/users/me', auth, async (req, res) => {
+  const updates = Object.keys(req.body);
+  const updateLists = ['fname', 'lname', 'email', 'master_password'];
+
+  const isAllowed = updates.every(update => updateLists.includes(update));
+
+  if (!isAllowed) {
+    res.status(404).send({ error: 'Invalid updates!' });
+  }
+
+  if (!req.user) {
+    return res.status(404).send();
+  }
+
+  try {
+    updates.forEach(update => (req.user[update] = req.body[update]));
+    req.user.save();
+    res.send(req.user);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
+router.delete('/users/me', auth, async (req, res) => {
+  try {
+    // await User.findByIdAndDelete(req.user._id);
+    await req.user.remove();
+    res.send(req.user);
+  } catch (error) {
+    res.status(400).send();
+  }
 });
 
 module.exports = router;
